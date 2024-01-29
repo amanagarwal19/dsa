3808d9d46244ae2ddc4b36e7afffb5e6f8e6d312
Aman Agarwal <90938481+amanagarwal19@users.noreply.github.com>
2024-01-28 23:25:22 -0600

    Time: 20 ms (46.03%) | Memory: 26.4 MB (17.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,23 +2,29 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int left[n];
-        left[0] = nums[0];
-        int right[n];
-        right[n-1] = nums[n-1];
-        for(int i=1;i<n;i++)
-            left[i] = left[i-1]*nums[i];
-        for(int i=n-2;i>=0;i--)
-            right[i] = right[i+1]*nums[i];
-        
         vector<int> ans(n,0);
+        ans[0] = nums[0];
+        for(int i=1;i<n;i++)
+            ans[i] = ans[i-1]*nums[i];
+        cout<<"\n";
+        for(int i=n-2;i>=0;i--)
+            nums[i] = nums[i+1]*nums[i];
 
+        int temp=ans[0];
         for(int i=0;i<n;i++){
             if(i==0)
-                ans[i] = right[i+1];
+            { 
+                temp=ans[i];
+                ans[i] = nums[i+1];
+            }
             else if(i==n-1)
-                ans[i] = left[i-1];
-            else ans[i] = left[i-1]*right[i+1];
+                ans[i] = temp;
+            else
+            {
+                int new_val = temp*nums[i+1];
+                temp = ans[i];
+                ans[i] = new_val;
+            } 
         }
         return ans;
     }
