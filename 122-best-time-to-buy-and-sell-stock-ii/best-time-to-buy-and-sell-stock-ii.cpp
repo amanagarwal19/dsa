1d280b89fdecbfade4de587867e74698a0a59d8a
Aman Agarwal <90938481+amanagarwal19@users.noreply.github.com>
2024-01-29 23:53:31 -0600

    Time: 6 ms (52.73%) | Memory: 15.4 MB (36.10%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,9 +4,22 @@ public:
         int n = prices.size();
         if(n<=1) return 0;
         int profit = 0;
-        for(int i=0;i<n-1;i++){
-            if(prices[i] < prices[i+1])
-                profit = profit + prices[i+1] - prices[i];
+        int peak=0,valley=0;
+        int i=0;
+        while(i<n){
+            //find valley prices[i] < prices[i+1]
+            // loop terminates when 2,3
+            while(i<n-1 && prices[i]>=prices[i+1])
+                i++;
+            valley = prices[i];
+
+            //find peak: loop terminates when 4,2
+            while(i<n-1 && prices[i]<=prices[i+1]) //2 , 3
+                i++;
+            peak = prices[i];
+            profit = max(profit,profit+(peak-valley));
+
+            i++;
         }
         return profit;
     }
