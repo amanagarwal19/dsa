6b5fde14009b30953915b3573e9c6b03d30fc9c7
Aman Agarwal <90938481+amanagarwal19@users.noreply.github.com>
2024-02-02 21:36:20 -0600

    Time: 0 ms (100.00%) | Memory: 7.5 MB (20.02%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        int i=0,j=0;
         int n = haystack.length();
-        int m = needle.length();
-        if(n<m) return -1;
-
         int windowStart=0;
-        int i=0;
-        while(i<m){   // traverse the needle
-            if(needle[i]!=haystack[windowStart+i]){ // mismatch, restart window
-                windowStart+=1;
-                i=0;
+
+        while(i<n){
+            if(haystack[i]==needle[j]) //character match
+            {
+                if(j==0) //start of window
+                    windowStart = i;
+                i++; //check for next character
+                j++;
+
+                //this condition be below increment of j because j 
+                //is checked for finishing all characters
+                if(j==needle.length()) 
+                    return windowStart;
+
             }
-            if(needle[i]==haystack[windowStart+i])
-                i++;
-            if(windowStart>(n-m)) break;    // no more space left to check
+            else{ //if not found
+                windowStart = windowStart + 1;
+                i = windowStart; //restart check from next window
+                j=0;
+            }
         }
-        if(i==m) 
-            return windowStart;
-        else
-            return -1;
+        return -1;
     }
 };
