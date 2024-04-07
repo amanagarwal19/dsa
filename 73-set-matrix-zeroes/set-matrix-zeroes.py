5eb67793de7477c7dd9d5a08cfb84d996663e13a
Aman Agarwal <90938481+amanagarwal19@users.noreply.github.com>
2024-04-06 19:13:57 -0500

    Time: 106 ms (38.74%) | Memory: 17.5 MB (78.09%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,35 +5,25 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
-        col0 = -1
 
-        # set top row as 0 for columns marking
-        # set left column as 0 for row marking
-        # use col0 to address the 0th column
+        row0=[None]*n
+        col0=[None]*m
+
         for i in range(m):
             for j in range(n):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    if j==0:
-                        col0=0
-                    else:
-                        matrix[0][j]=0
+                if(matrix[i][j]==0):
+                    row0[j] = 0;
+                    col0[i] = 0;
 
-        # fill from (1,1) to (m-1,n-1)
-        # So that we dont modify the top row/column
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0] == 0 or matrix[0][j]==0:
-                    matrix[i][j]=0
-
-        # fill the first row based on [0][0]
-        if(matrix[0][0]==0):
-            for j in range(0,n):
-                matrix[0][j] = 0
+        for j in range(n):
+            if(row0[j]==0):
+                for i in range(m):
+                    matrix[i][j] = 0;
         
-        # fill the first column based on col0 variable
-        if(col0==0):
-            for i in range(m):
-                matrix[i][0] = 0
+        for i in range(m):
+            if(col0[i]==0):
+                for j in range(n):
+                    matrix[i][j]=0;
+
 
         
